18dbfdf6c772d68f0289453e09bb397ba1d49d0a
Sean Hill <sean@tangentsolutions.co.za>
2017-07-04 14:26:09 +0200

    Login controller

---
+++ angularjsWebapiSean/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using angularjsWebapiSean.Client;
+using angularjsWebapiSean.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace angularjsWebapiSean.Controllers
+{
+    [Route("api/login")]
+    public class LoginController : Controller
+    {
+        private Client.Client client;
+        public LoginController(IClient _client)
+        {
+            client = (Client.Client)_client;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<bool> Post([FromBody]string username, [FromBody]string password)
+        {
+            LoginModel login = new LoginModel()
+            {
+                username = "jacob.zuma",
+                password = "tangent"
+            };
+            try { 
+                String token = await client.getToken(login);
+                if(token != null)
+                {
+                    return true;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            return false;
+        }
+    }
+}
