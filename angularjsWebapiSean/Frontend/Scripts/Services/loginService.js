7c0da4abe7884bd7cb21bdeb8a3beb4785f873a5
Sean Hill <sean@tangentsolutions.co.za>
2017-07-06 14:35:32 +0200

    loginService made async

--- angularjsWebapiSean/Frontend/Scripts/Services/loginService.js
+++ angularjsWebapiSean/Frontend/Scripts/Services/loginService.js
@@ -1,12 +1,17 @@
-﻿app.factory('LoginService', function ($http) {
+﻿app.factory('LoginService', function ($http, $q) {
     loginFunctions = {}
     loginFunctions.login = function (user) {
-        alert(user);
-        $http.post('/api/login?username=' + user.username + '&password=' + user.password).then(function(res){
-            console.log(res);
-        }
+        return $q(function (resolve, reject) {
+            $http.post('/api/login?username=' + user.username + '&password=' + user.password).then(function (res) {
 
-        );
+                resolve(res.data);
+            },
+                function (err) {
+                    reject(err);
+                }
+
+            );
+        });
     }
     return loginFunctions;
 
