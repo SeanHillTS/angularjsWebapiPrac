ff706a759267ef713acdba0a16679292f17ebf78
Sean Hill <sean@tangentsolutions.co.za>
2017-07-06 18:23:45 +0200

    edit row on click. Buggy width

--- angularjsWebapiSean/Frontend/Scripts/Controllers/projectsController .js
+++ angularjsWebapiSean/Frontend/Scripts/Controllers/projectsController .js
@@ -4,7 +4,7 @@ app.controller('projectsCtrl', function ($scope, $window, $location, ProjectsSer
     $scope.title = 'Projects come here';
 
     $scope.projects = {};
-
+    $scope.selectedPk = -1;
     
     var token = $window.sessionStorage.getItem("token");
     if (token == null) {
@@ -14,8 +14,20 @@ app.controller('projectsCtrl', function ($scope, $window, $location, ProjectsSer
     ProjectsService.getProjects(token).then(function (res) {
         console.log(res);
         $scope.projects = res;
+    });
+
+    $scope.editToggle = function (selected) {
+
+        console.log("call editToggle ", selected);
+        //Actually need a save(maybe)
+        if ($scope.selectedPk == selected)
+        {
+            $scope.selectedPk = -1;
+        }
+        else {
+            $scope.selectedPk = selected;
+        }
     }
-        )
 
 });
 
